5af32c16ec5ed03f7cee8cd168e36b99c66fe5fd
Jayasudha Jayakumaran <jayasudha.jayakumaran@coinbase.com>
2023-03-17 11:51:21 -0700

    release 0.1.0

---
+++ ios/PoolServiceBridge.m
@@ -0,0 +1,18 @@
+
+// PoolServiceBridge.m
+#import <React/RCTBridgeModule.h>
+
+@interface RCT_EXTERN_MODULE(PoolService, NSObject)
+
+RCT_EXTERN_METHOD(initialize: (NSString)apiKeyName
+                  withPrivateKey:(NSString)privateKey
+                  withResolver:(RCTPromiseResolveBlock)resolve
+                  withRejecter:(RCTPromiseRejectBlock)reject)
+
+
+RCT_EXTERN_METHOD(createPool:(NSString)displayName
+                  withPoolID:(NSString)poolID
+                  withResolver:(RCTPromiseResolveBlock)resolve
+                  withRejecter:(RCTPromiseRejectBlock)reject)
+
+@end
