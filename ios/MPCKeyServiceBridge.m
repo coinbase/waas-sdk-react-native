4f86ec90ebc744606ce8d46057493348478c3ad5
Jayasudha Jayakumaran <jayasudha.jayakumaran@coinbase.com>
2023-03-20 09:38:07 -0700

    add waas-sdk-react-native

---
+++ ios/MPCKeyServiceBridge.m
@@ -0,0 +1,50 @@
+// MPCKeyServiceBridge.m
+#import <React/RCTBridgeModule.h>
+
+@interface RCT_EXTERN_MODULE(MPCKeyService, NSObject)
+
+RCT_EXTERN_METHOD(initialize:(NSString)apiKeyName
+                  withPrivateKey:(NSString)privateKey
+                  withResolver:(RCTPromiseResolveBlock)resolve
+                  withRejecter:(RCTPromiseRejectBlock)reject)
+
+RCT_EXTERN_METHOD(registerDevice:(RCTPromiseResolveBlock)resolve
+                  withRejecter:(RCTPromiseRejectBlock)reject)
+
+RCT_EXTERN_METHOD(pollForPendingDeviceGroup:(NSString)deviceGroup
+                  withPollInterval:(nonnull NSNumber)pollInterval
+                  withResolver:(RCTPromiseResolveBlock)resolve
+                  withRejecter:(RCTPromiseRejectBlock)reject)
+
+RCT_EXTERN_METHOD(stopPollingForPendingDeviceGroup:(RCTPromiseResolveBlock)resolve
+                  withRejecter:(RCTPromiseRejectBlock)reject)
+
+RCT_EXTERN_METHOD(createTxSignature:(NSString)keyName
+                  withTx: (NSDictionary)tx
+                  withResolver:(RCTPromiseResolveBlock)resolve
+                  withRejecter:(RCTPromiseRejectBlock)reject)
+
+RCT_EXTERN_METHOD(createSignatureFromTx:(NSString)parent
+                  withTx:(NSDictionary)tx
+                  withResolver:(RCTPromiseResolveBlock)resolve
+                  withRejecter:(RCTPromiseRejectBlock)reject)
+
+RCT_EXTERN_METHOD(pollForPendingSignatures:(NSString)deviceGroup
+                  withPollInterval:(nonnull NSNumber)pollInterval
+                  withResolver:(RCTPromiseResolveBlock)resolve
+                  withRejecter:(RCTPromiseRejectBlock)reject)
+
+RCT_EXTERN_METHOD(stopPollingForPendingSignatures:(RCTPromiseResolveBlock)resolve
+                  withRejecter:(RCTPromiseRejectBlock)reject)
+
+
+RCT_EXTERN_METHOD(waitPendingSignature:(NSString)operation
+                  withResolver:(RCTPromiseResolveBlock)resolve
+                  withRejecter:(RCTPromiseRejectBlock)reject)
+
+RCT_EXTERN_METHOD(getSignedTransaction:(NSDictionary)tx
+                  withSignature:(NSDictionary)signature
+                  withResolver:(RCTPromiseResolveBlock)resolve
+                  withRejecter:(RCTPromiseRejectBlock)reject)
+
+@end
